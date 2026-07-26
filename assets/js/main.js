fb1744ba5fa769854a50151f73713e38b4d8d3de
zeafiola-ops <zeafiola@gmail.com>
2026-07-26 19:54:49 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-// JavaScript Utama
+document.getElementById("btnLogin").addEventListener("click", function () {
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "perpustakaan123" && password === "adminkey") {
+
+        alert("Login Berhasil!");
+
+        // Nanti diarahkan ke Dashboard
+        // window.location.href = "dashboard.html";
+
+    } else {
+
+        alert("Username atau Password Salah!");
+
+    }
+
+});
