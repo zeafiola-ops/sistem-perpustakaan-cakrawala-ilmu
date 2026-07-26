402f6d48574825e4bd2f0bb990f108ff02ddd47d
zeafiola-ops <zeafiola@gmail.com>
2026-07-26 20:06:20 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-alert("JavaScript Berhasil Terbaca!");
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("btnLogin").addEventListener("click", function () {
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        if (username === "perpustakaan123" && password === "adminkey") {
+
+            alert("Login Berhasil!");
+
+        } else {
+
+            alert("Username atau Password Salah!");
+
+        }
+
+    });
+
+});
